63743b9247e824872938effc464a21ed43ec1cdc
miko djatmiko <godofnjuk@gmail.com>
2021-11-11 05:12:57 +0700

    a

--- src/index.js
+++ src/index.js
@@ -171,7 +171,7 @@ app.get('/login',async (req, res) => {
   console.log("got a call from Wix for login");
   console.log("=============================");
 
-  const authorizationCode = req.query.id;
+  const authorizationCode = req.query.code;
 
   console.log("authorizationCode = " + authorizationCode);
 
