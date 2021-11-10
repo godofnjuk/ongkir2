45382c5f7791912e048b2c28f874e0d7cf7d0d79
miko djatmiko <godofnjuk@gmail.com>
2021-11-10 08:36:41 +0700

    a

--- src/credentials.js
+++ src/credentials.js
@@ -1,4 +1,4 @@
 // TODO: Update APP_SECRET with your application secret key (can be found in Wix Developers under Workspace/OAuth)
-const APP_SECRET = '9f585cf2-4d13-40e2-b95a-2bbd813c97a9';
-
+//const APP_SECRET = '9f585cf2-4d13-40e2-b95a-2bbd813c97a9';
+const APP_SECRET = 'c7c0d390-4328-4144-b314-dc662e8fba9d';
 exports.APP_SECRET = APP_SECRET;
