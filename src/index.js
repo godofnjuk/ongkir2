30ee8f010ac664e6e197fee8595c93c7c1a33b0e
miko djatmiko <godofnjuk@gmail.com>
2021-11-24 07:04:59 +0700

    a

--- src/index.js
+++ src/index.js
@@ -149,7 +149,7 @@ app.post('/order', async(req, res) => {
   console.log('webhook event data after verification:', prettyData);
   console.log("ordernya : "+JSON.stringify(prettyData.data.data));
   console.log("cart id : "+JSON.stringify(prettyData.data.data.cartId));
-  console.log("isi cart : "+JSON.stringify(getCart(prettyData.data.data.cartId)));
+  console.log("isi cart : "+getCart(prettyData.data.data.cartId));
   beratku = prettyData.data.data.totals.weight;
   //console.log(prettyData.data.data.billingInfo);
   //console.log(prettyData.data.data.billingInfo.address.city);
